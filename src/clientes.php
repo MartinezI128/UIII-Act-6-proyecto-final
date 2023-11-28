1ab78538c2b8bfcb0479fdff745da1ad42fb0d69
MartinezI128 <147106433+MartinezI128@users.noreply.github.com>
2023-11-27 19:07:35 -0700

    Add files via upload

---
+++ src/clientes.php
@@ -0,0 +1,189 @@
+<?php
+session_start();
+include "../conexion.php";
+$id_user = $_SESSION['idUser'];
+$permiso = "clientes";
+$sql = mysqli_query($conexion, "SELECT p.*, d.* FROM permisos p INNER JOIN detalle_permisos d ON p.id = d.id_permiso WHERE d.id_usuario = $id_user AND p.nombre = '$permiso'");
+$existe = mysqli_fetch_all($sql);
+if (empty($existe) && $id_user != 1) {
+    header('Location: permisos.php');
+}
+
+if (!empty($_POST)) {
+    $alert = "";
+    if (empty($_POST['nombre']) || empty($_POST['telefono']) || empty($_POST['direccion'])) {
+        $alert = '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+                        Todo los campos son obligatorio
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>';
+    } else {
+        $id = $_POST['id'];
+        $nombre = $_POST['nombre'];
+        $telefono = $_POST['telefono'];
+        $direccion = $_POST['direccion'];
+        $estado = $_POST['estado'];
+        $ciudad = $_POST['ciudad'];
+        $cp = $_POST['cp'];
+        $correo = $_POST['correo'];
+        $result = 0;
+        if (empty($id)) {
+            $query = mysqli_query($conexion, "SELECT * FROM cliente WHERE nombre = '$nombre'");
+            $result = mysqli_fetch_array($query);
+            if ($result > 0) {
+                $alert = '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+                        El cliente ya existe
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>';
+            } else {
+                $query_insert = mysqli_query($conexion, "INSERT INTO cliente(nombre,telefono,direccion,estado,ciudad,cp,correo) values ('$nombre', '$telefono', '$direccion', '$estado', '$ciudad', '$cp', '$correo')");
+                if ($query_insert) {
+                    $alert = '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                        Cliente registrado
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>';
+                } else {
+                    $alert = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        Error al registrar
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>';
+                }
+            }
+        }else{
+            $sql_update = mysqli_query($conexion, "UPDATE cliente SET nombre = '$nombre' , telefono = '$telefono', direccion = '$direccion', estado = '$estado', ciudad = '$ciudad', cp = '$cp', correo = '$correo' WHERE idcliente = $id");
+            if ($sql_update) {
+                $alert = '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                        Cliente Modificado
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>';
+            } else {
+                $alert = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        Error al modificar
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>';
+            }
+        }
+    }
+    mysqli_close($conexion);
+}
+include_once "includes/header.php";
+?>
+<div class="card">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-md-12">
+                <?php echo (isset($alert)) ? $alert : '' ; ?>
+                <form action="" method="post" autocomplete="off" id="formulario">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label for="nombre" class="text-dark font-weight-bold">Nombre</label>
+                                <input type="text" placeholder="Ingrese Nombre" name="nombre" id="nombre" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <label for="telefono" class="text-dark font-weight-bold">Teléfono</label>
+                                <input type="number" placeholder="Ingrese Teléfono" name="telefono" id="telefono" class="form-control">
+                                <input type="hidden" name="id" id="id">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label for="direccion" class="text-dark font-weight-bold">Dirección</label>
+                                <input type="text" placeholder="Ingrese Direccion" name="direccion" id="direccion" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label for="estado" class="text-dark font-weight-bold">Estado</label>
+                                <input type="text" placeholder="Ingrese Estado" name="estado" id="estado" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label for="ciudad" class="text-dark font-weight-bold">Ciudad</label>
+                                <input type="text" placeholder="Ingrese Ciudad" name="ciudad" id="ciudad" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label for="cp" class="text-dark font-weight-bold">Codigo Postal</label>
+                                <input type="text" placeholder="Ingrese Codigo Postal" name="cp" id="cp" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label for="correo" class="text-dark font-weight-bold">Correo</label>
+                                <input type="email" placeholder="Ingrese Correo" name="correo" id="correo" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-md-4 mt-3">
+                            <input type="submit" value="Registrar" class="btn btn-primary" id="btnAccion">
+                            <input type="button" value="Nuevo" class="btn btn-success" id="btnNuevo" onclick="limpiar()">
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-12">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered" id="tbl">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th>#</th>
+                                <th>Nombre</th>
+                                <th>Teléfono</th>
+                                <th>Dirección</th>
+                                <th>Estado</th>
+                                <th>Ciudad</th>
+                                <th>CP</th>
+                                <th>Correo</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            include "../conexion.php";
+
+                            $query = mysqli_query($conexion, "SELECT * FROM cliente");
+                            $result = mysqli_num_rows($query);
+                            if ($result > 0) {
+                                while ($data = mysqli_fetch_assoc($query)) { ?>
+                                    <tr>
+                                        <td><?php echo $data['idcliente']; ?></td>
+                                        <td><?php echo $data['nombre']; ?></td>
+                                        <td><?php echo $data['telefono']; ?></td>
+                                        <td><?php echo $data['direccion']; ?></td>
+                                        <td><?php echo $data['estado']; ?></td>
+                                        <td><?php echo $data['ciudad']; ?></td>
+                                        <td><?php echo $data['cp']; ?></td>
+                                        <td><?php echo $data['correo']; ?></td>
+                                        <td>
+                                            <a href="#" onclick="editarCliente(<?php echo $data['idcliente']; ?>)" class="btn btn-primary"><i class='fas fa-edit'></i></a>
+                                            <form action="eliminar_cliente.php?id=<?php echo $data['idcliente']; ?>" method="post" class="confirmar d-inline">
+                                                <button class="btn btn-danger" type="submit"><i class='fas fa-trash-alt'></i> </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                            <?php }
+                            } ?>
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include_once "includes/footer.php"; ?>
